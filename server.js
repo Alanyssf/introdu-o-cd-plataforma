88c2dedf7225ddb68eb16b6c55139dbd19d6bf1f
alanyssf <alanyssfsantos13@gmail.com>
2026-05-22 09:30:50 -0300

    Meu primeiro código com automação de CD

--- server.js
+++ server.js
@@ -9,8 +9,3 @@ app.get('/', (req, res) => {
 app.listen(PORT, () => {
     console.log('Servidor rodando na porta ' + PORT);
 });
-8.	4. Abra o arquivo package.json que foi criado automaticamente. Encontre a seção "scripts" e adicione um script para simular um teste e outro para rodar o servidor, deixando assim:
-"scripts": {
-  "test": "echo \"Todos os testes passaram com sucesso!\" && exit 0",
-  "start": "node server.js"
-}
